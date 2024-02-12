44f41377fa7eb45290f9f1dd7ca778068143c88d
sri1686 <sri1686>
2024-02-12 03:35:13 +0000

    2nd commit

--- Untitled Notebook 2024-02-11 22_00_51.py
+++ Untitled Notebook 2024-02-11 22_00_51.py
@@ -1,17 +1,34 @@
 # Databricks notebook source
-# Mounting code
-dbutils.fs.mount(
-  source="wasbs://input@storageaccount1686.blob.core.windows.net/",
-  mount_point="/mnt/my-mount",
-  extra_configs={
-    "fs.azure.account.key.storageaccount1686.blob.core.windows.net": "7FpgEb0knKTvY/5alNx9XKeD7XIhxlAiPMgqN678qDTl0ntj8OL/uo3ZnBHzcCPQTWo4DpEQQiLA+AStFaM13Q=="
-  }
-)
+# # Mounting code
+# dbutils.fs.mount(
+#   source="wasbs://input@storageaccount1686.blob.core.windows.net/",
+#   mount_point="/mnt/my-mount",
+#   extra_configs={
+#     "fs.azure.account.key.storageaccount1686.blob.core.windows.net": "7FpgEb0knKTvY/5alNx9XKeD7XIhxlAiPMgqN678qDTl0ntj8OL/uo3ZnBHzcCPQTWo4DpEQQiLA+AStFaM13Q=="
+#   }
+# )
 
 # COMMAND ----------
 
 dbutils.fs.ls("/mnt/my-mount")
 
+# COMMAND ----------
+
+df=spark.read.csv("dbfs:/mnt/my-mount/sample.csv")
+
+# COMMAND ----------
+
+# Correct usage for writing a DataFrame to a Parquet file
+df.write.csv("dbfs:/mnt/my-mount/out_sample.csv")
+
+
+# COMMAND ----------
+
+df=spark.read.csv("dbfs:/mnt/my-mount/out_sample.csv")
+
+# COMMAND ----------
+
+df.display()
 
 # COMMAND ----------
 
